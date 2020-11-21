1f089b01fe9085878c18c3dc18a179555168906b
asd10384 <65000786+asd10384@users.noreply.github.com>
2020-11-21 23:48:23 +0900

    12

--- meal.js
+++ meal.js
@@ -6,10 +6,6 @@ const cheerio = require('cheerio');
 
 async function getHTML() {
     try {
-        $.ajax({
-            url: '',
-            headers: { 'Access-Control-Allow-Origin': '*' },
-        });
         return await axios.get('https://school.busanedu.net/bsnamil-h/main.do');
     } catch (error) {
         console.error(error);
