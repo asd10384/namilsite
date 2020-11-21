ffda0192cd9c6bc0569e49a75afa8c058df99743
asd10384 <65000786+asd10384@users.noreply.github.com>
2020-11-21 14:29:44 +0900

    12

--- meal.js
+++ meal.js
@@ -1,35 +1,35 @@
-function mealjs() {
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-    async function getHTML() {
-        try {
-            return await get('http://school.busanedu.net/bsnamil-h/main.do');
-        } catch (error) {
-            console.error(error);
-        }
+async function getHTML() {
+    try {
+        return await axios.get('http://school.busanedu.net/bsnamil-h/main.do');
+    } catch (error) {
+        console.error(error);
     }
-
-    getHTML()
-        .then(html => {
-            let HTMLList = [];
-            const $ = load(html.data);
-            const bodyList = $('div.widgDiv.meal_menu1095 ul').children('li');
-
-            bodyList.each(function(i, elem) {
-                HTMLList[i] = {
-                    kcal: $(this)
-                        .find('dt.kcal span')
-                        .text(),
-                    meal: $(this)
-                        .find('dd.meal_list')
-                        .text()
-                };
-            });
-            return HTMLList;
-        })
-        .then(res => {
-            let kcal = res[0].kcal.slice(0, -4);
-            let meal = res[0].meal;
-            return meallist = [kcal, meal];
-        });
-        return alert('12');
 }
+
+getHTML()
+    .then(html => {
+        let HTMLList = [];
+        const $ = cheerio.load(html.data);
+        const bodyList = $('div.widgDiv.meal_menu1095 ul').children('li');
+
+        bodyList.each(function(i, elem) {
+            HTMLList[i] = {
+                kcal: $(this)
+                    .find('dt.kcal span')
+                    .text(),
+                meal: $(this)
+                    .find('dd.meal_list')
+                    .text()
+            };
+        });
+        return HTMLList;
+    })
+    .then(res => {
+        let kcal = res[0].kcal.slice(0, -4);
+        let meal = res[0].meal;
+
+        return meallist = [kcal, meal];
+    });
