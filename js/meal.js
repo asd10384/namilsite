30906a5a50713a44ba3cfaa0a3b9611c85a4e26a
asd10384 <65000786+asd10384@users.noreply.github.com>
2020-11-25 21:55:58 +0900

    12

--- js/meal.js
+++ js/meal.js
@@ -10,27 +10,4 @@ async function getHTML() {
     }
 }
 
-getHTML()
-    .then(html => {
-        let HTMLList = [];
-        const $ = cheerio.load(html.data);
-        const bodyList = $('div.widgDiv.meal_menu1095 ul').children('li');
-
-        bodyList.each(function(i, elem) {
-            HTMLList[i] = {
-                kcal: $(this)
-                    .find('dt.kcal span')
-                    .text(),
-                meal: $(this)
-                    .find('dd.meal_list')
-                    .text()
-            };
-        });
-        return HTMLList;
-    })
-    .then(res => {
-        let kcal = res[0].kcal.slice(0, -4);
-        let meal = res[0].meal;
-
-        return meallist = [kcal, meal];
-    });
+export { getHTML };
