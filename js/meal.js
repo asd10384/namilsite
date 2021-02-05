c731ad16a739a361920105e8960cad4d616b5d7c
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-02-06 03:07:09 +0900

    add node_module

--- js/meal.js
+++ js/meal.js
@@ -1,12 +1,44 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+import { get } from '../node_modules/axios/index';
+import { load } from '../node_modules/cheerio/index';
 
 async function getHTML() {
     try {
-        return await axios.get('https://school.busanedu.net/bsnamil-h/main.do');
+        var url = `http://school.busanedu.net/bsnamil-h/main.do`;
+        return await get(url, {
+            headers: {
+                'Access-Control-Allow-Credentials': true,
+                'Access-Control-Allow-Origin':'*',
+                'Access-Control-Allow-Methods':'GET'
+            }
+        });
     } catch (error) {
         console.error(error);
     }
 }
-
-export { getHTML };
+async function serHTML() {
+    getHTML()
+        .then(html => {
+            let HTMLList = [];
+            const $ = load(html.data);
+            const bodyList = $('div.widgDiv.meal_menu1095 ul').children('li');
+    
+            bodyList.each(function(i, elem) {
+                HTMLList[i] = {
+                    kcal: $(this)
+                        .find('dt.kcal span')
+                        .text(),
+                    meal: $(this)
+                        .find('dd.meal_list')
+                        .text()
+                };
+            });
+            return HTMLList;
+        })
+        .then(res => {
+            let kcal = res[0].kcal.slice(0, -4);
+            let meal = res[0].meal;
+            
+            document.getElementById('kcal').innerHTML = kcal;
+            document.getElementById('meal').innerHTML = meal;
+        });
+}
