f23ce1bb3b9e36c6b6241f2ba4b85a80cf42b22b
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-02-09 15:30:50 +0900

    최종

--- js/openfile.js
+++ js/openfile.js
@@ -5,16 +5,16 @@ function openfile(list, id, title, discription, page, par) {
             // 읽어올문서의 파일명, 확인용 함수(완료여부)
             $(`#${id}`).load(`${par}`, function (txt, status) {
                 if (status == 'error') {
-                    $(`#${id}`).html(`<link rel="stylesheet" href="./csss/err.css" />
+                    $(`#${id}`).html(`<link rel="stylesheet" href="./css/err.css" />
                     <div id="err">
                         <p>파일을 찾을수 없습니다.</p>
                     </div>`);
                 } else {
                     txt = `<link rel="stylesheet" href="./css/${page}.css" />
                     <meta name="image" content="https://namilsite.netlify.app/images/${page}.png" />
-                    <meta property="url" content="https://namilsite.netlify.app/indexcopy.html?page=${page}" />
+                    <meta property="url" content="https://namilsite.netlify.app/index.html?page=${page}" />
                     
-                    <meta property="og:url" content="https://namilsite.netlify.app/indexcopy.html?page=${page}" />
+                    <meta property="og:url" content="https://namilsite.netlify.app/index.html?page=${page}" />
                     <meta property="og:image" content="https://namilsite.netlify.app/images/${page}.png" />
                     <meta property="og:image:type" content="image/png" />
                     <meta property="og:image:alt" content="${title}" />
@@ -25,7 +25,7 @@ function openfile(list, id, title, discription, page, par) {
                 }
             });
         } else {
-            $(`#${id}`).html(`<link rel="stylesheet" href="./csss/err.css" />
+            $(`#${id}`).html(`<link rel="stylesheet" href="./css/err.css" />
             <div id="err">
                 <p>파일을 찾을수 없습니다.</p>
             </div>`);
