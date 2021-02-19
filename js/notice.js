78baf4d6034934733cad23956b8309aee6985ed7
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-02-20 05:14:22 +0900

    사이트 알림 추가 (학교시간)

---
+++ js/notice.js
@@ -0,0 +1,49 @@
+
+window.onload = function () {
+    setInterval(function() {
+        if (window.Notification) {
+            Notification.requestPermission();
+        }
+        var d = new Date();
+        var week = d.getDay(); // 일요일부터 0~6
+        var hour = d.getHours();
+        var min = d.getMinutes();
+        var sec = d.getSeconds();
+
+        var ctl = {
+            8: 40,
+            9: 40,
+            10: 40,
+            11: 40,
+            13: 30,
+            14: 30,
+            15: 40,
+        };
+
+        // 토일요일 제거
+        if (week !== 0 || week !== 6) {
+            // 반목문 사용
+            for (i in ctl) {
+                if (hour === i) {
+                    if (min === ctl[i]-5 && sec === 0) {
+                        notify(hour, min);
+                    }
+                }
+            }
+        }
+    }, 1000);
+}
+
+function notify(hour = 0, min = 0) {
+    var ctl_n = Object.keys(ctl);
+    var t = ctl_n.indexOf(hour.toString()) + 1;
+    if (Notification.permission === 'granted') {
+        var n = new Notification(`수업 들어갈 시간입니다.`, {
+            icon: './images/logo.png',
+            body: `${t}교시 시작 5분전입니다.\n현재시간 : ${hour}시 ${min}분\n\n클릭시 줌번호 사이트로 이동`,
+        });
+        n.onclick = function () {
+            window.open('https://namilsite.netlify.app?page=zoomid');
+        };
+    }
+}
