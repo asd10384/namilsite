a525eb7123cafeb0a3ba4206a5cac41e71fd6c01
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-03-10 00:14:27 +0900

    파일위치 이동

--- js/classtime.js
+++ js/classtime.js
@@ -1,44 +1,46 @@
 
-// 제작된 반 리스트
-var slist = [
-    '3', 
-    '5', 
-    '6', 
-    '7'
-];
-
-var page = query().page;
-var clnum = query().class;
-if (page == 'classtime') {
-    if (clnum == undefined || clnum == '0') {
-        $('main_title').html(`ㆍ시간표 모음ㆍ`);
-        $('#classtime').html(`
-        자신의 반을 선택한 뒤,
-        <br/>
-        시간표를 확인하세요.
-        <br/><br/>
-        현재 추가된 반
-        <br/>
-        ${slist}
-        `);
-    } else {
-        $('main_title').html(`ㆍ${clnum}반 시간표ㆍ`);
-        if (slist.includes(clnum)) {
-            cl(clnum);
-        } else {
+document.addEventListener('DOMContentLoaded', function() {
+    // 제작된 반 리스트
+    var slist = [
+        '3', 
+        '5', 
+        '6', 
+        '7'
+    ];
+    
+    var page = query().page;
+    var clnum = query().class;
+    if (page == 'classtime') {
+        if (clnum == undefined || clnum == '0') {
+            $('main_title').html(`ㆍ시간표 모음ㆍ`);
             $('#classtime').html(`
-            ${clnum}반 시간표가 아직 추가되지 않았습니다.
+            자신의 반을 선택한 뒤,
+            <br/>
+            시간표를 확인하세요.
             <br/><br/>
-            (시간표추가를 원하시면 '2-5허승한'에게 연락해주세요.)
+            현재 추가된 반
+            <br/>
+            ${slist}
             `);
+        } else {
+            $('main_title').html(`ㆍ${clnum}반 시간표ㆍ`);
+            if (slist.includes(clnum)) {
+                cl(clnum);
+            } else {
+                $('#classtime').html(`
+                ${clnum}반 시간표가 아직 추가되지 않았습니다.
+                <br/><br/>
+                (시간표추가를 원하시면 '2-5허승한'에게 연락해주세요.)
+                `);
+            }
         }
     }
-}
+});
 
 function cl(s) {
     // txt 파일 가져오기 (load)
     // 읽어올문서의 파일명, 확인용 함수(완료여부)
-    $('#classtime').load(`../file/room${s}.txt`, function(txt, status) {
+    $('#classtime').load(`./file/room${s}.txt`, function(txt, status) {
         if (status == 'error') {
             $('#classtime').text('파일을 찾을 수 없습니다.');
         } else {
