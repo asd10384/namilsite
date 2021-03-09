dd23e17df07d793a552ba6779aa04e882dec0fa1
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-03-09 20:31:17 +0900

    오류수정 최종

--- js/classtime.js
+++ js/classtime.js
@@ -29,7 +29,7 @@ if (page == 'classtime') {
 function cl(s) {
     // txt 파일 가져오기 (load)
     // 읽어올문서의 파일명, 확인용 함수(완료여부)
-    $('#classtime').load(`./file/room${s}.txt`, function(txt, status) {
+    $('#classtime').load(`../file/room${s}.txt`, function(txt, status) {
         if (status == 'error') {
             $('#classtime').text('파일을 찾을 수 없습니다.');
         } else {
