60c0ebddde898ec768e1765b499c1ca6cfc63b88
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-03-10 22:41:47 +0900

    급식페이지 제작

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     var list = [
-        'err',
         'html',
         'clock',
         'zoomid',
         'classtime',
-        'selecttime'
+        'meal',
+        'selecttime',
     ];
     var name = [
-        '에러',
         '사이트 모음',
         '시계',
         '줌번호',
         '시간표 모음',
-        '선택&합반 시간표'
+        '급식',
+        '선택&합반 시간표',
     ];
     var text = query().page;
     if (text == undefined) {
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
         var title = `ㆍ${name[n]}ㆍ`;
     } else {
         text = 'err';
-        var title = name[0];
+        var title = `에러`;
     }
     document.getElementById("main_title").innerHTML = title;
     openfile(list, 'main', title, '남일고 사이트 모음', text, `../html/${text}.html`);
---
+++ js/meal.js
@@ -0,0 +1,37 @@
+
+var day_list = {
+    '0': '일',
+    '1': '월',
+    '2': '화',
+    '3': '수',
+    '4': '목',
+    '5': '금',
+    '6': '토',
+};
+var md = new Date();
+$("#date").html(`${
+    md.getMonth() + 1
+}월 ${
+    md.getDate()
+}일 ${
+    md.getDay().toString().replace(/[0-9]/, (daynum) => day_list[daynum])
+}요일 급식 (중식)`);
+const headers = new Headers({
+    'Content-Type': 'text/xml',
+});
+fetch('http://cors-anywhere.herokuapp.com/http://school.busanedu.net/bsnamil-h/main.do', { headers }).then((response) => {
+    const a = response.text().then((text) => {
+        text = text.replace(/\s/g, '').split(/>/);
+        var kcal = text[1228].slice(0,-9);
+        var meal_list = text[1231].slice(0,-4)
+            .replace(/[0-9]/g,'')
+            .replace(/\.+$/g,'')
+            .split(/\.+/g);
+        var mealtext = '';
+        for (i in meal_list) {
+            mealtext += `${meal_list[i]}<br/>`;
+        }
+        $("#kcal").html(`${kcal}kcal`);
+        $("#meal_list").html(`${mealtext}`);
+    });
+});
