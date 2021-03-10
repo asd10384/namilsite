6ae6beaa03aa2e0362739f6b8547e4c779911088
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-03-10 23:51:22 +0900

    파일열기 최적화

--- js/main.js
+++ js/main.js
@@ -16,17 +16,23 @@ document.addEventListener('DOMContentLoaded', function() {
         '급식',
         '선택&합반 시간표',
     ];
-    var text = query().page;
-    if (text == undefined) {
-        text = 'html';
+    var page = query().page;
+    var cl = query().class;
+    if (page == undefined) {
+        page = 'html';
     }
-    var n = list.indexOf(text);
+    var n = list.indexOf(page);
     if (n > -1) {
         var title = `ㆍ${name[n]}ㆍ`;
+        if (page == 'classtime') {
+            if (cl > 0) {
+                title = `ㆍ${cl}반 시간표ㆍ`;
+            }
+        }
     } else {
-        text = 'err';
+        page = 'err';
         var title = `에러`;
     }
     document.getElementById("main_title").innerHTML = title;
-    openfile(list, 'main', title, '남일고 사이트 모음', text, `../html/${text}.html`);
+    openfile('main', page, `../html/${page}.html`);
 });
--- js/openfile.js
+++ js/openfile.js
@@ -1,26 +1,20 @@
-function openfile(list, id, title, discription, page, par) {
-    $(function () {
-        if (list.indexOf(page) > -1) {
-            // txt 파일 가져오기 (load)
-            // 읽어올문서의 파일명, 확인용 함수(완료여부)
-            $(`#${id}`).load(`${par}`, function (txt, status) {
-                if (status == 'error') {
-                    $(`#${id}`).html(`<link rel="stylesheet" href="./css/err.css" />
-                    <div id="err">
-                        <p>파일을 찾을수 없습니다.</p>
-                    </div>`);
-                } else {
-                    txt = `<link rel="stylesheet" href="./css/${page}.css" />
 
-                    ${txt}`;
-                    $(`#${id}`).html(txt);
-                }
-            });
-        } else {
-            $(`#${id}`).html(`<link rel="stylesheet" href="./css/err.css" />
-            <div id="err">
-                <p>파일을 찾을수 없습니다.</p>
-            </div>`);
-        }
+function openfile(id, page, par) {
+    $(function () {
+        // txt 파일 가져오기 (load)
+        // 읽어올문서의 파일명, 확인용 함수(완료여부)
+        $(`#${id}`).load(`${par}`, function (txt, status) {
+            if (status == 'error') {
+                $(`#${id}`).html(`<link rel="stylesheet" href="./css/err.css" />
+                <div id="err">
+                    <p>파일을 찾을수 없습니다.</p>
+                </div>`);
+            } else {
+                txt = `<link rel="stylesheet" href="./css/${page}.css" />
+
+                ${txt}`;
+                $(`#${id}`).html(txt);
+            }
+        });
     });
 }
