147996b5c57c49e1bed7148fde5c4de0acf7794c
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-03-29 14:30:58 +0900

    리턴값 수정

--- js/notice.js
+++ js/notice.js
@@ -64,11 +64,23 @@ window.onload = function () {
     }, 1000);
 }
 
-function notify({hour = '8', min = 37, at = 3, t = 1, test = false}) {
-    var text = (test) ? '<br/>(이 알림은<br/>테스트 알림 입니다.)' : ''
+function notify({hour = '8', min = 37, at = 3, t = 1, audio = "./sound/notice.mp3", test = false}) {
+    var text = (test) ? '<br/>(이 알림은<br/>테스트 알림 입니다.)' : '';
+    new Audio(audio).play();
     toastr.success(
         `현재시간 : ${hour}시 ${min}분${text}`,
         `${t}교시 시작 ${at}분전입니다.`,
     );
-    new Audio("./sound/notice.mp3").play();
+    var rt = [
+        true, 
+        {
+            hour: hour,
+            min: min,
+            at: at,
+            t: t,
+            test: test,
+            audio: audio
+        }
+    ];
+    return rt;
 }
