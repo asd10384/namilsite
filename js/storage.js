952994db88ab218a39be9fd2cfaa775c17f4b0e7
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-04-07 21:01:43 +0900

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -9,9 +9,11 @@ if (page != 'classset') {
 
 function setclassnum() {
     var num = document.getElementById('classnum').value;
+    console.log(num);
     if (num < 0 || num > 8) {
         return alert('반은 1~8반까지 있습니다.');
     }
+    console.log(num);
     alert(`반을 ${num}반으로 설정합니다.\n'내정보'->'반변경' 에서 반을 변경하실수 있습니다.`);
     localStorage.setItem('classnum', num);
     location.href = './index.html';
