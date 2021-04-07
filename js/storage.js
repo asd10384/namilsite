a3aa0dd74da1c1f36317889cd85bbc480bd9d19a
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-04-07 21:23:50 +0900

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -9,7 +9,7 @@ if (page != 'classset') {
 
 function setclassnum() {
     var num = document.getElementById('classnum').value;
-    if (num < 0 && num > 8 && num != '' && !(isNaN(num))) {
+    if (num < 0 && num > 8 && (num == '' || isNaN(num))) {
         return alert('반은 1~8반까지 있습니다.');
     }
     alert(`반을 ${num}반으로 설정합니다.\n'내정보'->'반변경' 에서 반을 변경하실수 있습니다.`);
