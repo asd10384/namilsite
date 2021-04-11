4f0bcd96c8955c18f8d9822a19dd3467f1f32b46
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-04-11 20:16:40 +0900

    test4

--- js/test_openfile.js
+++ js/test_openfile.js
@@ -1,19 +1,25 @@
 
 function openfile(id, page, par) {
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            el.outerHTML = this.responseText;
-        } else {
-            $(`#pagecss`).attr(`href`, `./css/err.css`);
-            $(`#${id}`).html(`
-                <div id="err">
-                    <p>파일을 찾을수 없습니다.</p>
-                </div>
-            `);
+    var allElements = document.getElementsByTagName('*');
+    console.log(allElements);
+    Array.prototype.forEach.call(allElements, (el) => {
+        if (par) {
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    el.outerHTML = this.responseText;
+                } else {
+                    $(`#pagecss`).attr(`href`, `./css/err.css`);
+                    $(`#${id}`).html(`
+                        <div id="err">
+                            <p>파일을 찾을수 없습니다.</p>
+                        </div>
+                    `);
+                }
+            };
+            $(`#pagecss`).attr(`href`, `./css/${page}.css`);
+            xhttp.open('GET', par, true);
+            xhttp.send();
         }
-    };
-    $(`#pagecss`).attr(`href`, `./css/${page}.css`);
-    xhttp.open('GET', par, true);
-    xhttp.send();
+    });
 }
