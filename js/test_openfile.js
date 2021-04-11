7966e1ab97b97929f7f5ea25b23adde4566c6caa
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-04-11 20:53:55 +0900

    Update test_openfile.js

--- js/test_openfile.js
+++ js/test_openfile.js
@@ -1,21 +1,36 @@
 
 function openfile(id, page, par) {
     var xhttp = new XMLHttpRequest();
-    if (!(this.readyState == 4 && this.status == 200)) {
-        $(`#pagecss`).attr(`href`, `./css/${page}.css`);
-    } else {
-        $(`#pagecss`).attr(`href`, `./css/err.css`);
-        $(`#${id}`).html(`
-            <div id="err">
-                <p>파일을 찾을수 없습니다.</p>
-            </div>
-        `);
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            $(`#pagecss`).attr(`href`, `./css/${page}.css`);
+        } else {
+            $(`#pagecss`).attr(`href`, `./css/err.css`);
+            $(`#${id}`).html(`
+                <div id="err">
+                    <p>파일을 찾을수 없습니다.</p>
+                </div>
+            `);
+        }
     }
     xhttp.open('GET', par, true);
-    xhttp.send().then(xhttp => {
-        console.log(xhttp);
-    });
+    xhttp.send();
     document.getElementById('main').innerHTML = xhttp.response;
+
+    // if (!(this.readyState == 4 && this.status == 200)) {
+    //     $(`#pagecss`).attr(`href`, `./css/${page}.css`);
+    // } else {
+    //     $(`#pagecss`).attr(`href`, `./css/err.css`);
+    //     $(`#${id}`).html(`
+    //         <div id="err">
+    //             <p>파일을 찾을수 없습니다.</p>
+    //         </div>
+    //     `);
+    // }
+    // xhttp.open('GET', par, true);
+    // xhttp.send();
+    // document.getElementById('main').innerHTML = xhttp.response;
+
     // var allElements = document.getElementsByTagName('*');
     // Array.prototype.forEach.call(allElements, (el) => {
     //     console.log(el);
