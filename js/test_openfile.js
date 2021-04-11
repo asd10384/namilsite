f8dc0fb738692b816ebbd50780ef5800148e0999
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-04-11 20:27:44 +0900

    test6

--- js/test_openfile.js
+++ js/test_openfile.js
@@ -1,25 +1,39 @@
 
 function openfile(id, page, par) {
     var xhttp = new XMLHttpRequest();
-    var allElements = document.getElementsByTagName('*');
-    Array.prototype.forEach.call(allElements, (el) => {
-        console.log(el);
-        if (par) {
-            xhttp.onreadystatechange = function () {
-                if (this.readyState == 4 && this.status == 200) {
-                    el.outerHTML = this.responseText;
-                    $(`#pagecss`).attr(`href`, `./css/${page}.css`);
-                } else {
-                    $(`#pagecss`).attr(`href`, `./css/err.css`);
-                    $(`#${id}`).html(`
-                        <div id="err">
-                            <p>파일을 찾을수 없습니다.</p>
-                        </div>
-                    `);
-                }
-            };
-            xhttp.open('GET', par, true);
-            xhttp.send();
-        }
-    });
+    var el = document.getElementById('#main');
+    if (this.readyState == 4 && this.status == 200) {
+        el.outerHTML = this.responseText;
+        $(`#pagecss`).attr(`href`, `./css/${page}.css`);
+    } else {
+        $(`#pagecss`).attr(`href`, `./css/err.css`);
+        $(`#${id}`).html(`
+            <div id="err">
+                <p>파일을 찾을수 없습니다.</p>
+            </div>
+        `);
+    }
+    xhttp.open('GET', par, true);
+    xhttp.send();
+    // var allElements = document.getElementsByTagName('*');
+    // Array.prototype.forEach.call(allElements, (el) => {
+    //     console.log(el);
+    //     if (par) {
+    //         xhttp.onreadystatechange = function () {
+    //             if (this.readyState == 4 && this.status == 200) {
+    //                 el.outerHTML = this.responseText;
+    //                 $(`#pagecss`).attr(`href`, `./css/${page}.css`);
+    //             } else {
+    //                 $(`#pagecss`).attr(`href`, `./css/err.css`);
+    //                 $(`#${id}`).html(`
+    //                     <div id="err">
+    //                         <p>파일을 찾을수 없습니다.</p>
+    //                     </div>
+    //                 `);
+    //             }
+    //         };
+    //         xhttp.open('GET', par, true);
+    //         xhttp.send();
+    //     }
+    // });
 }
