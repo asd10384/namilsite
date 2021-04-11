5a415ab6d8c67fb7ca08b93647e1d7912ab724b6
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-04-11 21:44:35 +0900

    autozoomid index폴더에 추가

--- js/autozoomid.js
+++ js/autozoomid.js
@@ -7,70 +7,72 @@ if (classnum == undefined || classnum == '0') {
     location.href = '../index.html?page=classset';
 }
 
-$(function () {
-    var week = weeklist[new Date().getDay()];
-    $(`#zoomid`).load(`../file/zoomid.json`, function (txt, status) {
-        if (status == 'error') {
-            $(`#zoomid`).show();
-            $(`#zoomid`).html(`
-                <link rel="stylesheet" href="./css/err.css" />
-                <div id="err">
-                    <p>파일을 찾을수 없습니다.</p>
-                </div>
-            `);
-        } else {
-            $(`#zoomcode`).hide();
-            $(`#zoomid`).hide();
-            const zoomid = JSON.parse(txt);
-            $(`#classtime`).load(`../file/room${classnum}.json`, function (rtxt, status) {
-                if (status == 'error') {
-                    $(`#zoom`).html(`
-                        <p>
-                            ${classnum}반 시간표가 아직
-                            <br/>
-                            추가되지 않았습니다.
-                        </p>
-                    `);
-                } else {
-                    $(`#classtime`).hide();
-                    const classtime = JSON.parse(rtxt);
-                    const classtime_name1 = Object.keys(classtime);
-                    var chtml = '';
-                    var zoomidtxt;
-
-                    if (classtime_name1.indexOf(week) > -1) {
-                        chtml += `${week}요일 시간표`;
-                    }
-                    else if (['토','일'].includes(week)) {
-                        var nowweek = week;
-                        week = '월';
-                        chtml += `${nowweek}요일은 ${week}요일 시간표를<br/>미리 볼수있습니다.`;
-                    }
-                    const classtime_name2 = Object.keys(classtime[week]);
-                    for (i in classtime_name2) {
-                        var cltxt = classtime[week][classtime_name2[i]];
-                        var cllist = cltxt.replace('(','').replace(')','').split(' ');
-                        if (cllist[0] == undefined || cllist[0] == '') continue;
-                        try {
-                            zoomidtxt = zoomid[cllist[0]][cllist[1]];
-                        } catch(err) {
-                            zoomidtxt = undefined;
+function loadzoomid() {
+    $(function () {
+        var week = weeklist[new Date().getDay()];
+        $(`#zoomid`).load(`../file/zoomid.json`, function (txt, status) {
+            if (status == 'error') {
+                $(`#zoomid`).show();
+                $(`#zoomid`).html(`
+                    <link rel="stylesheet" href="./css/err.css" />
+                    <div id="err">
+                        <p>파일을 찾을수 없습니다.</p>
+                    </div>
+                `);
+            } else {
+                $(`#zoomcode`).hide();
+                $(`#zoomid`).hide();
+                const zoomid = JSON.parse(txt);
+                $(`#classtime`).load(`../file/room${classnum}.json`, function (rtxt, status) {
+                    if (status == 'error') {
+                        $(`#zoom`).html(`
+                            <p>
+                                ${classnum}반 시간표가 아직
+                                <br/>
+                                추가되지 않았습니다.
+                            </p>
+                        `);
+                    } else {
+                        $(`#classtime`).hide();
+                        const classtime = JSON.parse(rtxt);
+                        const classtime_name1 = Object.keys(classtime);
+                        var chtml = '';
+                        var zoomidtxt;
+    
+                        if (classtime_name1.indexOf(week) > -1) {
+                            chtml += `${week}요일 시간표`;
                         }
-                        chtml += `<div id="clt"><a id="clt1">${Number(i)+1}교시 </a><a id="clt2">${cltxt}</a><br/><a id="clt3"`;
-                        if (zoomidtxt == undefined || zoomidtxt == '-') {
-                            zoomidtxt = `
-                                zoom번호 사이트에서<br/>직접 입력해주세요.`;
-                        } else {
-                            chtml += `href="#" onclick="gozoom('${zoomidtxt}')"`;
+                        else if (['토','일'].includes(week)) {
+                            var nowweek = week;
+                            week = '월';
+                            chtml += `${nowweek}요일은 ${week}요일 시간표를<br/>미리 볼수있습니다.`;
                         }
-                        chtml += `>${zoomidtxt}</a></div>`;
+                        const classtime_name2 = Object.keys(classtime[week]);
+                        for (i in classtime_name2) {
+                            var cltxt = classtime[week][classtime_name2[i]];
+                            var cllist = cltxt.replace('(','').replace(')','').split(' ');
+                            if (cllist[0] == undefined || cllist[0] == '') continue;
+                            try {
+                                zoomidtxt = zoomid[cllist[0]][cllist[1]];
+                            } catch(err) {
+                                zoomidtxt = undefined;
+                            }
+                            chtml += `<div id="clt"><a id="clt1">${Number(i)+1}교시 </a><a id="clt2">${cltxt}</a><br/><a id="clt3"`;
+                            if (zoomidtxt == undefined || zoomidtxt == '-') {
+                                zoomidtxt = `
+                                    zoom번호 사이트에서<br/>직접 입력해주세요.`;
+                            } else {
+                                chtml += `href="#" onclick="gozoom('${zoomidtxt}')"`;
+                            }
+                            chtml += `>${zoomidtxt}</a></div>`;
+                        }
+                        $(`#zoom`).html(chtml);
                     }
-                    $(`#zoom`).html(chtml);
-                }
-            });
-        }
+                });
+            }
+        });
     });
-});
+}
 
 function gozoom(url = '000-000-0000') {
     console.log(url);
